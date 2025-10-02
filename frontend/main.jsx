d9a7f6a56ed4cc04a64d4ed8172fb80ad1887f18
Gallo <gallogeta@gmail.com>
2025-10-03 00:50:35 +0300

    Force main bundle cache invalidation for contact form fix - v3

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+// Cache invalidation: Contact form fix v3
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
